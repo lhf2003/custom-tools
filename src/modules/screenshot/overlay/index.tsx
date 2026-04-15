acf9c132039c4b7d2955fc9539cdba59500b32cf
LHF <2385107101@qq.com>
2026-04-15 15:42:05 +0800

    fix: 截图遮罩窗口添加 ToastContainer 以显示成功提示

--- src/modules/screenshot/overlay/index.tsx
+++ src/modules/screenshot/overlay/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ScreenshotOverlay from './ScreenshotOverlay';
+import { ToastContainer } from '@/components/Toast';
 import '@/index.css';
 import './overlay.css';
 
@@ -14,6 +15,7 @@ if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <ScreenshotOverlay />
+      <ToastContainer />
     </React.StrictMode>,
   );
   console.log('[ScreenshotOverlay] React app mounted');
